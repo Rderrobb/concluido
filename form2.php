d216c7c606800cee6bc3f26ac687ff3174d8f9ac
Rderrobb <125322933+Rderrobb@users.noreply.github.com>
2023-06-19 19:30:21 -0300

    Update exibir2.php

--- form2.php
+++ form2.php
@@ -1,36 +1,58 @@
+<link rel="stylesheet" href="style.css">
 <?php
 
 
 $Pessoa = $_POST['Pessoa'];
 
-
-
 if ($Pessoa == "PessoaAdministrativo"){
     require_once("PessoaAdministrativo.class.php");
     
+
     $Nome = $_POST['Nome'];
     $Identificacao = $_POST['Identificacao'];
     $Setor = $_POST['Setor'];
+    $Bloco = $_POST['$Bloco'];
+    
+    $Pessoa1 = new PessoaAdministrativo($Nome, $Identificacao, $Bloco, $Setor );
+    
+    echo $Pessoa1->VerPessoa();
+
+  
+
+   
+
+}
+
+elseif ($Pessoa == "PessoaDocente"){
+    require_once("PessoaDocente.class.php");
+
+    $Nome = $_POST['Nome'];
+    $Identificacao = $_POST['Identificacao'];
     $Bloco = $_POST['Bloco'];
+    $Disciplina = $_POST['$Disciplina'];
+
+    $Pessoa2 = new PessoaDocente($Nome, $Identificacao, $Bloco, $Disciplina);
+
+    echo $Pessoa2->VerPessoa();
+
+}
+
+else {
+    require_once("PessoaDiscente.class.php");
+
+
+    $Nome = $_POST['Nome'];
+    $Identificacao = $_POST['Identificacao'];
+    $Turno = $_POST['Turno$Turno'];
+    $Curso = $_POST['$Curso'];
+
+   $Pessoa3 = new PessoaDiscente($Nome, $Identificacao, $Turno, $Curso);
+
+    echo $Pessoa3->VerPessoa();
 
     
-
-    echo "<h1>Seja bem vindo a area administrativa $Nome, identificação: $Identificacao</h1>";
-
-    echo "<html>";
-    echo "<head>";
-    echo "<link rel='stylesheet' href='style.css'>";
-    echo "<title>Formulario 2</title>";
-    echo "</head>";
-    echo "<body>";
-    echo "<form action='exibir.php'>";
-    echo "<label>Setor: </label><input type='text' name='Setor' value='Setor'>";
-    echo "<label>Bloco: </label><input type='text' name='Bloco' value='Bloco'>";
-    echo "<input type='submit' value='enviar'>";
-    echo "</form>";
-
-
-
+  
+    
 }
 
 
