d2fd68aa04d8fa12998be56fd7fc62ae4bf72c15
Rderrobb <125322933+Rderrobb@users.noreply.github.com>
2023-06-25 18:30:08 -0300

    Update Pessoa.class.php

--- Pessoa.class.php
+++ Pessoa.class.php
@@ -4,21 +4,19 @@ class Pessoa{
 
     public $Nome;
     public $Identificacao;
-    
+    public $Sexo;
 
     function __construct($Nome, $Identificacao){
-        
         $this->Nome = $Nome;
         $this->Identificacao = $Identificacao;
-        
-
+        $this->Sexo = $Sexo;
     }
 
     function VerPessoa(){
 
         echo "<b>Nome: </b>{$this->Nome}<br/><br/>";
         echo "<b>Identificacao: </b>{$this->Identificacao}<br/><br/>";
-        
+        echo "<b>Sexo: </b>{$this->Sexo}<br/><br/>";
     }
 
 }
